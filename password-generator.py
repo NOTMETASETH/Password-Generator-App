ff169ba8e1fccd7a0245680a8b5058bda837c991
Seth Goodley <SethGoodley@protonmail.com>
2023-06-30 18:50:36 -0400

    passwd generator working code

--- password-generator.py
+++ password-generator.py
@@ -1,14 +1,14 @@
 import string
 import random
-
+from csv import writer
+    
 def password_generator():
     lower_case = string.ascii_lowercase
     upper_case = string.ascii_uppercase
     digits = string.digits
     symbols = string.punctuation
-    
+    platform = input("Enter the name of the platform: \n")
     pass_length = int(input("Enter the length of the password: \n"))
-    
     s = []
     s.extend(list(lower_case))
     s.extend(list(upper_case))
@@ -17,5 +17,9 @@ def password_generator():
     random.shuffle(s)
     password = ("".join(s[0:pass_length]))
     print(password)
+    passdata= [platform,password]
+    with open ('pass.csv','a') as f:
+        writedata = writer(f)
+        writedata.writerow(passdata)
 
 password_generator()
